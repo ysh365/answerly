06e6acb8612cc17638080a8e3225b632f7e50dd0
ysh <102450911+ysh365@users.noreply.github.com>
2024-10-19 14:25:01 +0800

    first commit

---
+++ src/main/java/org/buaa/project/controller/UserController.java
@@ -0,0 +1,19 @@
+package org.buaa.project.controller;
+
+/*用户管理控制层*/
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+
+    /*根据用户名查找用户信息*/
+    @GetMapping("/api/answerly/v1/user/{username}")
+    public String getUser(@PathVariable String username) {
+        return "Hello " + username;
+    }
+
+}
