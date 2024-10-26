4966f76ad9d96c559ca2eaff2478096e7a7a697b
方沐阳 <fmy21373061@buaa.edu.cn>
2024-10-26 22:24:11 +0800

    增加delete和like接口

--- src/main/java/org/buaa/project/service/QuestionService.java
+++ src/main/java/org/buaa/project/service/QuestionService.java
@@ -2,10 +2,13 @@ package org.buaa.project.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import org.buaa.project.dao.entity.QuestionDO;
+import org.buaa.project.dto.req.QuestionUploadReqDTO;
 
 import java.util.List;
 
 public interface QuestionService extends IService<QuestionDO> {
     //上传题目
-    Boolean uploadQuestion(int Category, String content, String title, int idUser, List<String> pictures);
+    Boolean uploadQuestion(QuestionUploadReqDTO params);
+    Boolean deleteQuestion(int id);
+    Boolean likeQuestion(int id);
 }
