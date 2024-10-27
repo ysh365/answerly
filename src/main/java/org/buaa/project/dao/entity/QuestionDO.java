95868ec82766317f15682b21351118f691a86be4
ysh <102450911+ysh365@users.noreply.github.com>
2024-10-27 17:42:51 +0800

    数据库持久层对象基础属性

--- src/main/java/org/buaa/project/dao/entity/QuestionDO.java
+++ src/main/java/org/buaa/project/dao/entity/QuestionDO.java
@@ -1,15 +1,12 @@
 package org.buaa.project.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-
-import java.util.Date;
+import org.buaa.project.common.database.BaseDO;
 
 @Data
 @TableName("question")
-public class QuestionDO {
+public class QuestionDO extends BaseDO {
     /**
      * id
      */
@@ -38,37 +35,17 @@ public class QuestionDO {
     /**
      * 浏览量
      */
-    @TableField(fill = FieldFill.INSERT)
     private Integer viewCount;
 
     /**
      * 点赞数
      */
-    @TableField(fill = FieldFill.INSERT)
     private Integer likeCount;
 
     /**
      * 是否解决 0：未解决 1：已解决
      */
-    @TableField(fill = FieldFill.INSERT)
     private int solvedFlag;
 
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-
-    /**
-     * 修改时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0：未删除 1：已删除
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private int delFlag;
 
 }
--- src/main/java/org/buaa/project/dao/entity/UserDO.java
+++ src/main/java/org/buaa/project/dao/entity/UserDO.java
@@ -1,18 +1,15 @@
 package org.buaa.project.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-
-import java.util.Date;
+import org.buaa.project.common.database.BaseDO;
 
 /**
  * 用户持久层实体
  */
 @Data
 @TableName("user")
-public class UserDO {
+public class UserDO extends BaseDO {
     /**
      * id
      */
@@ -63,21 +60,4 @@ public class UserDO {
      */
     private int status;
 
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-
-    /**
-     * 修改时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0：未删除 1：已删除
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Integer delFlag;
 }
