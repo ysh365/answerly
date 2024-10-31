c6f070898f3f4f8d0988dc431bf5f6905464e2b5
ysh <102450911+ysh365@users.noreply.github.com>
2024-10-31 23:16:50 +0800

    封装用户上下文

---
+++ src/main/java/org/buaa/project/common/biz/user/UserInfoDTO.java
@@ -0,0 +1,38 @@
+package org.buaa.project.common.biz.user;
+
+import com.alibaba.fastjson2.annotation.JSONField;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 用户信息实体
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserInfoDTO {
+
+    /**
+     * 用户 ID
+     */
+    @JSONField(name = "id")
+    private String userId;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 学生类别
+     */
+    private String userType;
+
+    /**
+     * Token
+     */
+    private String token;
+}
---
+++ src/main/java/org/buaa/project/common/biz/user/UserTransmitFilter.java
@@ -0,0 +1,92 @@
+package org.buaa.project.common.biz.user;
+
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.buaa.project.common.convention.exception.ClientException;
+import org.buaa.project.common.convention.result.Results;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Objects;
+
+import static org.buaa.project.common.consts.RedisCacheConstant.USER_LOGIN_KEY;
+import static org.buaa.project.common.enums.UserErrorCodeEnum.USER_TOKEN_NULL;
+
+/**
+ * 用户信息传输过滤器
+ */
+@RequiredArgsConstructor
+public class UserTransmitFilter implements Filter {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/answerly/v1/user/login",
+            "/api/answerly/v1/user/has-username",
+            "/api/answerly/v1/user/send-code"
+    );
+
+    @SneakyThrows
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String requestURI = httpServletRequest.getRequestURI();
+        if (!IGNORE_URI.contains(requestURI)) {
+            String method = httpServletRequest.getMethod();
+            if (!(Objects.equals(requestURI, "/api/answerly/v1/user") && Objects.equals(method, "POST"))) {
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                if (!StrUtil.isAllNotBlank(username, token)) {
+                    returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_NULL))));
+                    return;
+                }
+                Object userInfoJsonStr;
+                try {
+                    userInfoJsonStr = stringRedisTemplate.opsForHash().get(USER_LOGIN_KEY + username, token);
+                    if (userInfoJsonStr == null) {
+                        throw new ClientException(USER_TOKEN_NULL);
+                    }
+                } catch (Exception ex) {
+                    returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_NULL))));
+                    return;
+                }
+                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+                userInfoDTO.setToken(token);
+                UserContext.setUser(userInfoDTO);
+            }
+        }
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            UserContext.removeUser();
+        }
+    }
+
+    private void returnJson(HttpServletResponse response, String json) throws Exception {
+        PrintWriter writer = null;
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try {
+            writer = response.getWriter();
+            writer.print(json);
+
+        } catch (IOException e) {
+        } finally {
+            if (writer != null)
+                writer.close();
+        }
+    }
+
+}
